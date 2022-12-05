2b85b176b81f7039b0c2fdf10aedb3d9caaf221e
Albert Pham <pnphuong29@gmail.com>
2022-12-05 18:36:51 +0700

    Add hotkey for ShowNoteProperties command

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { MenuItemLocation } from 'api/types';
 joplin.plugins.register({
     onStart: async function () {
         await joplin.commands.register({
-            name: 'testCmd',
-            label: 'Test command',
+            name: 'apMoveToFolder',
+            label: 'Move to folder',
             execute: async (noteIds: string[]) => {
                 // If this command is triggered from menu <Tools> then noteIds will be null
                 // So we need to get all selected note ids in current workspace as below
@@ -13,24 +13,50 @@ joplin.plugins.register({
                     noteIds = await joplin.workspace.selectedNoteIds();
                 }
 
-                noteIds.forEach(async (noteId: string) => {
-                    await joplin.commands.execute('moveToFolder', noteIds);
-                });
+                await joplin.commands.execute('moveToFolder', noteIds);
+            },
+        });
+
+        await joplin.commands.register({
+            name: 'apShowNoteProperties',
+            label: 'Show note properties',
+            execute: async (noteId: string) => {
+                // If this command is triggered from menu <Tools> then noteId will be null
+                // So we need to get selected note id in current workspace as below
+                if (!noteId) {
+                    noteId = (await joplin.workspace.selectedNote()).id;
+                }
+
+                await joplin.commands.execute('showNoteProperties', noteId);
             },
         });
 
         await joplin.views.menuItems.create(
-            'ToolsTestCommand',
-            'testCmd',
+            'ToolsApMoveToFolder',
+            'apMoveToFolder',
             MenuItemLocation.Tools,
             { accelerator: 'CmdOrCtrl+Shift+E' }
         );
 
         await joplin.views.menuItems.create(
-            'noteListMenuItemTestCommand',
-            'testCmd',
+            'noteListMenuItemApMoveToFolder',
+            'apMoveToFolder',
             MenuItemLocation.NoteListContextMenu,
             { accelerator: 'CmdOrCtrl+Shift+E' }
+        );
+
+        await joplin.views.menuItems.create(
+            'ToolsApShowNoteProperties',
+            'apShowNoteProperties', 
+            MenuItemLocation.Tools,
+            { accelerator: 'CmdOrCtrl+Shift+I' }
+        );
+
+        await joplin.views.menuItems.create(
+            'noteListMenuItemApShowNoteProperties',
+            'apShowNoteProperties', 
+            MenuItemLocation.NoteListContextMenu,
+            { accelerator: 'CmdOrCtrl+Shift+I' }
         );
     },
 });
